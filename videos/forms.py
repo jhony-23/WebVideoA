3d56519dadefccdc94d6ad5e850abfccb52e8ce2
jhony_23 <ixc221231@uvg.edu.gt>
2025-08-25 11:18:16 -0600

    Cambios en estilos y aoariencia

--- videos/forms.py
+++ videos/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from .models import Video
 
+class CustomClearableFileInput(forms.ClearableFileInput):
+    template_with_initial = (
+        '%(initial_text)s: %(initial)s <br>'
+        '%(input_text)s: %(input)s'
+    )
+    initial_text = 'Actualmente'
+    input_text = 'Reemplazar'
+
 class VideoForm(forms.ModelForm):
     class Meta:
         model = Video
-        fields = ('title', 'videofile')  # Usa los nombres del modelo
+        fields = ('title', 'videofile')
         labels = {
             'title': 'Título',
             'videofile': 'Cargar video',
+        }
+        widgets = {
+            'videofile': CustomClearableFileInput,
         }
